009afd5a093e1b2a7033cf9134394ba250e455fc
NguyenThiYen03032006 <nguyenthiyen03032006@gmail.com>
2024-12-20 01:14:42 +0700

    Add files via upload

---
+++ bai4_ss18.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+struct SinhVien{
+    int id;
+    char name[50];
+    int age;
+    char phoneNumber[15];
+};
+typedef struct SinhVien sv;
+int main(void){
+    sv arrSv[50];
+    for(int i=0; i<5; i++){
+        arrSv[i].id=i+1;
+        printf("Moi ban nhap ten sv thu %d :", i+1);
+        fgets(arrSv[i].name, 50, stdin);
+        // xoa dau \n
+        for(int k=0; k< 50; k++){
+            if(arrSv[i].name[k] =='\n'){
+                arrSv[i].name[k]='\0';
+            }
+        }
+        
+        printf("Moi ban nhap tuoi sv thu %d :", i+1);
+        scanf("%d", &arrSv[i].age);
+        getchar();
+        printf("Moi ban nhap sdt sv thu %d :", i+1);
+        fgets(arrSv[i].phoneNumber, 15, stdin);
+    }
+    for(int i=0;i<5; i++){
+        printf("%d \t", arrSv[i].id);
+        printf("%s \t", arrSv[i].name);
+        printf("%d \t", arrSv[i].age);
+        printf("%s \t", arrSv[i].phoneNumber);
+        printf("\n");
+    }
+    return 0;
+}
+
